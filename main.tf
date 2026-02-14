8c07c368c844f556157b9a0e3aaf8f02c2cb9103
Josh VanderLinden <codekoala@gmail.com>
2026-02-14 11:33:58 -0500

    Change required fields

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ resource "aws_cognito_user_pool" "this" {
     attribute_data_type      = "String"
     developer_only_attribute = false
     mutable                  = true
-    required                 = true
+    required                 = false
 
     string_attribute_constraints {
       min_length = 2
@@ -71,7 +71,7 @@ resource "aws_cognito_user_pool" "this" {
     attribute_data_type      = "String"
     developer_only_attribute = false
     mutable                  = true
-    required                 = true
+    required                 = false
 
     string_attribute_constraints {
       min_length = 2
